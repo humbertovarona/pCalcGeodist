f185a1fd2765ba930e040b005cb7d1c307ed39a6
Humberto L. Varona <humberto.varona@yandex.com>
2023-05-30 20:40:09 -0300

    Create calculate_distance.py

---
+++ calculate_distance.py
@@ -0,0 +1,10 @@
+def calculate_distance(lat1, lon1, lat2, lon2):
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2) ** 2
+    distance = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return distance
